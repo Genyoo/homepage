488a074a02e6a795b94a12e4df2fd76b2dad3491
Hging <hging3@gmail.com>
2020-10-21 09:27:59 +0800

    first commit for genyoo

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -4,19 +4,15 @@ import React from 'react';
 import { enquireScreen } from 'enquire-js';
 
 import Nav0 from './Nav0';
-import Banner0 from './Banner0';
+import Banner1 from './Banner1';
 import Content0 from './Content0';
-import Content1 from './Content1';
-import Content3 from './Content3';
 import Footer0 from './Footer0';
 
 import {
   Nav00DataSource,
-  Banner00DataSource,
+  Banner10DataSource,
   Content00DataSource,
-  Content10DataSource,
-  Content30DataSource,
-  Footer01DataSource,
+  Footer00DataSource,
 } from './data.source';
 import './less/antMotionStyle.less';
 
@@ -25,7 +21,7 @@ enquireScreen((b) => {
   isMobile = b;
 });
 
-const { location } = window;
+const { location = {} } = typeof window !== 'undefined' ? window : {};
 
 export default class Home extends React.Component {
   constructor(props) {
@@ -62,10 +58,10 @@ export default class Home extends React.Component {
         dataSource={Nav00DataSource}
         isMobile={this.state.isMobile}
       />,
-      <Banner0
-        id="Banner0_0"
-        key="Banner0_0"
-        dataSource={Banner00DataSource}
+      <Banner1
+        id="Banner1_0"
+        key="Banner1_0"
+        dataSource={Banner10DataSource}
         isMobile={this.state.isMobile}
       />,
       <Content0
@@ -74,22 +70,10 @@ export default class Home extends React.Component {
         dataSource={Content00DataSource}
         isMobile={this.state.isMobile}
       />,
-      <Content1
-        id="Content1_0"
-        key="Content1_0"
-        dataSource={Content10DataSource}
-        isMobile={this.state.isMobile}
-      />,
-      <Content3
-        id="Content3_0"
-        key="Content3_0"
-        dataSource={Content30DataSource}
-        isMobile={this.state.isMobile}
-      />,
       <Footer0
-        id="Footer0_1"
-        key="Footer0_1"
-        dataSource={Footer01DataSource}
+        id="Footer0_0"
+        key="Footer0_0"
+        dataSource={Footer00DataSource}
         isMobile={this.state.isMobile}
       />,
     ];
